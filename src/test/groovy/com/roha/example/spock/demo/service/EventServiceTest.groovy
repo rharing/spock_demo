7a67a7670dce06287c1cd5b50d4ec4d7a0a92946
Ronald Haring <C59728 ronald.haring@nl.abnamro.com>
2019-11-04 11:00:44 +0100

    clean up

---
+++ src/test/groovy/com/roha/example/spock/demo/service/EventServiceTest.groovy
@@ -0,0 +1,53 @@
+package com.roha.example.spock.demo.service
+
+import com.roha.example.spock.demo.model.Event
+import org.joda.time.DateTime
+import spock.lang.Issue
+import spock.lang.Narrative
+import spock.lang.Specification
+import spock.lang.Title
+
+@Narrative("""
+i want to create events and users and invite users to events so that i have a better recollection who is coming with me
+""")
+@Title("crudding events")
+@Issue("EVE-001")
+class EventServiceTest extends Specification {
+    EventService eventService
+
+    def setup(){
+        eventService = new EventService()
+    }
+    def "should create event"(){
+        def eventDate = new DateTime(2019, 12, 4, 20, 0)
+        when: "an event is created"
+        Event event = eventService.createEvent("toutpartout", eventDate, true, "Whispering sons en meer")
+        List<Event> allEvents = eventService.listEvents()
+        def dayevents = eventService.getEvents(eventDate)
+        then:"the id of the event should be updated"
+        event.id != null
+        and:"all events should have a size of 1"
+        allEvents.size() == 1
+        and: "the events for this day should have a size of 1"
+        dayevents.size() == 1
+        when:"another event on another day is added"
+        event = eventService.createEvent("!!! (Chk Chk Chk)", eventDate.plusDays(1), false, "!!! (Chk Chk Chk)")
+        event.price= 20.5f
+        event.location = "Bitterzoet"
+        eventService.update(event)
+        allEvents = eventService.listEvents()
+        dayevents = eventService.getEvents(eventDate)
+        then: "the newly added event should have an id"
+        event.id != null
+        and:"there should be a total of 2 events"
+        allEvents.size() == 2
+        and: "but the day events for the specific date should still be one"
+        dayevents.size()==1
+        when: "retrieving event byId"
+        Event persisted =eventService.getById(event.id)
+        then: "event should have fields as defined"
+        persisted.id ==2
+        persisted.title == "!!! (Chk Chk Chk)"
+        persisted.price == 20.5f
+    }
+}
